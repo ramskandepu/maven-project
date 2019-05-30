52e463455a3e5db5f61fffb34f4af6cf5e64bc28
Ram Mohan <rams.kandepu@gmail.com>
2019-05-30 12:02:03 -0400

    Changed Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,8 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String final someone) {
+  //Add javadoc comment
+  public String greet(String someone) {
     return String.format("Hello everyone in the, %s!", someone);
   }
 }
